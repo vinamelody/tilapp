4a6674511b701d676d3b6659a5709b6dec3081af
Vina <vina_melody@yahoo.com>
2018-03-19 20:04:21 +0800

    Finishing CRUD

---
+++ Sources/App/Controllers/AcronymsController.swift
@@ -0,0 +1,40 @@
+import Vapor
+
+struct AcronymsController: RouteCollection {
+    func boot(router: Router) throws {
+        let acronymsRoute = router.grouped("api", "acronyms")
+        acronymsRoute.get(use: getAllHandler)
+        acronymsRoute.post(use: createHandler)
+        acronymsRoute.get(Acronym.parameter, use: getHandler)
+        acronymsRoute.delete(Acronym.parameter, use: deleteHandler)
+        acronymsRoute.put(Acronym.parameter, use: updateHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
+        return Acronym.query(on: req).all()
+    }
+    
+    func createHandler(_ req: Request) throws -> Future<Acronym> {
+        let acronym = try req.content.decode(Acronym.self).await(on: req)
+        return acronym.save(on: req)
+    }
+    
+    func getHandler(_ req: Request) throws -> Future<Acronym> {
+        return try req.parameter(Acronym.self)
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameter(Acronym.self).flatMap(to: HTTPStatus.self, { (acronym) in return acronym.delete(on: req).transform(to: .noContent)
+        })
+    }
+    
+    func updateHandler(_ req: Request) throws -> Future<Acronym> {
+        return try flatMap(to: Acronym.self, req.parameter(Acronym.self), req.content.decode(Acronym.self), { (acronym, updatedAcronym) in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req)
+        })
+    }
+}
+
+extension Acronym: Parameter {}
