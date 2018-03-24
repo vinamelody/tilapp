0cead7c71183d683dbccb18c9d6c6276fc7cb434
Vina <vina_melody@yahoo.com>
2018-03-24 14:53:29 +0800

    Create acronym single page from index list

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -4,13 +4,25 @@ import Leaf
 struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("acronyms", Acronym.parameter, use: acronymHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
         // Originally like below but thanks to the extension, it can be shorten to return try req.leaf().render("index")
 //        return try req.make(LeafRenderer.self).render("index")
-        let context = IndexContent(title: "Homepage")
-        return try req.leaf().render("index", context)
+        return Acronym.query(on: req).all().flatMap(to: View.self, { acronyms in
+            let context = IndexContent(title: "Homepage", acronyms: acronyms.isEmpty ? nil : acronyms)
+            return try req.leaf().render("index", context)
+        })
+    }
+    
+    func acronymHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameter(Acronym.self).flatMap(to: View.self, { acronym in
+            return acronym.creator.get(on: req).flatMap(to: View.self, { creator in
+                let context = AcronymContext(title: acronym.long, acronym: acronym, creator: creator)
+                return try req.leaf().render("acronym", context)
+            })
+        })
     }
 }
 
@@ -22,4 +34,11 @@ extension Request {
 
 struct IndexContent: Encodable {
     let title: String
+    let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let creator: User
 }
