923464f19abdefdb2f083782a079eb2baa0f9bdc
Vina <vina_melody@yahoo.com>
2018-03-26 20:36:39 +0800

    Embedding template and create user page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -5,6 +5,7 @@ struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        router.get("users", User.parameter, use: userHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -24,6 +25,15 @@ struct WebsiteController: RouteCollection {
             })
         })
     }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameter(User.self).flatMap(to: View.self) { user in
+            return try user.acronyms.query(on: req).all().flatMap(to: View.self) { acronyms in
+                let context = UserContext(title: user.name, user: user, acronyms: acronyms.isEmpty ? nil : acronyms)
+                return try req.leaf().render("user", context)
+            }
+        }
+    }
 }
 
 extension Request {
@@ -41,4 +51,10 @@ struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let creator: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]?
 }
